5620daabf7c2cc24d6bba331bb6d83eab3b8ed87
Sonoko <xuenan_li@brown.edu>
2017-04-08 18:24:36 -0400

    create a donor-profile module
    Use the IMGheader, create a new div containing both user profile photo and user basic info.
    Haven't come up with a responsive design

---
+++ app/js/components/donorProfile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import IMGheader from './IMGheader.jsx';
+
+export default React.createClass({
+    getInitialState: function() {
+        return {
+            imgsrc : 'test2.png'
+        };
+    },
+
+    render: function() {
+        return (
+            <div>
+                <IMGheader/>
+                <div className="rows">
+                    <div className="donorProfile col-lg-2 col-lg-offset-2 col-md-offset-1">
+                        <img src={this.state.imgsrc} alt="Image Not Found" className="img-thumbnail"/>
+                    </div>
+                    <div className="donorInfo panel panel-default col-lg-4">
+                        <div className="donorIntroduction panel-body">
+                            <h2>I am a donor</h2>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dui dignissim, rutrum felis ac, congue erat.
+                            Mauris volutpat ut nibh ut dictum. Curabitur vulputate facilisis nulla sed sodales. Maecenas mollis a est vel fringilla.
+                            Fusce tristique risus id ante rutrum, in suscipit nulla venenatis.
+                            </p>
+                        </div>
+                        <div className="donorToolbar">
+                            <span className="glyphicon glyphicon-user"></span>
+                            <span> 350 connections </span>
+                            <span><a href="#">view</a></span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
