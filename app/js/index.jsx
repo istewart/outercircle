0cbce24354c365fc7f620afad4b4fead75e5ca15
David Branse <David@Davids-MacBook-Pro-2.local>
2017-04-10 14:08:28 -0400

    fixed bug so pages can render

--- app/js/index.jsx
+++ app/js/index.jsx
@@ -8,19 +8,15 @@ import {
 
 import css from '../css/main.css';
 
-import Feed from './components/feed.jsx';
-import IMGheader from './components/IMGheader.jsx';
 import DonorPage from './components/donorPage.jsx';
-import Title from './components/title.jsx';
 import Home from './components/home.jsx';
-// Commenting this out because crashes since stats.jsx doesn't exist
-//import Stats from './components/stats.jsx';
+import Charity from './components/charity.jsx';
+
 
 const Routes = () => (
   <BrowserRouter>
     <div>
         <Route exact path='/' component={Home}/>
-        <Route path='/images' component={IMGheader}/>
         <Route path='/donor' component={DonorPage}/>
         <Route path='/charity' component={Charity}/>
     </div>
