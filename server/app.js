6b8f51b458b602bec5cd165262405f918fe7105d
Ian Stewart <istewart3141@gmail.com>
2017-04-16 23:44:20 -0400

    Set up a simple database schema and started on the API.

--- server/app.js
+++ server/app.js
@@ -1,30 +1,94 @@
 var express = require('express');
-// var anyDB = require('any-db'); // TODO:
+var anyDB = require('any-db'); // TODO:
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(express.static('dist')); // TODO: not sure about this
+
+// serve static files
+app.use(express.static('dist'));
+app.use(express.static('img'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// var db = anyDB.createPool( // TODO: do something here
-//   'sqlite3://chatroom.db',
-//   {min: 2, max: 8}
-// );
+// set up a connection pool
+var db = anyDB.createPool( // TODO: need to transition to postgres
+  'sqlite3://outercircle.db', // for prod
+  {min: 2, max: 8}
+);
 
-// Set up public access for images folder
-app.use(express.static("img"));
+// create a post for a given donor // TODO: security, cookies, auth
+app.post('/post', function(request, response) {
+  console.log('- Request received /post');
+  
+  const donor = request.body.donor;
+  const body = request.body.body;
+  const time = new Date().getTime();
 
+  var sql = 'INSERT INTO post (donor, body, time) \
+            VALUES (?, ?, ?)';
+  db.query(sql, [donor, body, time], function(error, result) {
+    // TODO: error handling and security and auth and xss and csrf
+    response.sendStatus(200);
+  });
+});
+
+// serve the home page on any other request // TODO: this is sketchy
 app.get('*', function(request, response) {
   console.log('- Request received *:');
-  response.sendFile('feed.html', {root : "dist"}); // TODO: again here
+  response.sendFile('feed.html', {root : 'dist'}); // TODO: again here
 });
 
-// start the web server
-app.listen(8080, function(){
-  console.log('- Server listening on port 8080');
-});
+// initialize the database the web server
+init(function() {
+  app.listen(8080, function(){
+    console.log('- Server listening on port 8080');
+  });
+})
 
+// initialize the database
+function init(callback) {
+  console.log('Initializing database...');
+
+  var sql = 'CREATE TABLE IF NOT EXISTS donor ( \
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    name TEXT, \
+    email TEXT, \
+    description TEXT, \
+    profile_image TEXT, \
+    cover_image TEXT \
+  );'
+
+  db.query(sql, function(error, result) {
+    console.log('Initialized donor table.');
+  });
+
+  var sql = 'CREATE TABLE IF NOT EXISTS charity ( \
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    name TEXT, \
+    website TEXT, \
+    description TEXT, \
+    cover_image TEXT \
+  );'
+
+  db.query(sql, function(error, result) {
+    console.log('Initialized donor table.');
+  });
+
+  sql = 'CREATE TABLE IF NOT EXISTS post ( \
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    donor INTEGER, \
+    body TEXT, \
+    time INTEGER, \
+    FOREIGN KEY(donor) REFERENCES donor(id) \
+  );'
+
+  db.query(sql, function(error, result) {
+    console.log('Initialized post table.');
+  });
+
+  // wait a second for things to finish then start the server
+  setTimeout(callback, 1000);
+}
