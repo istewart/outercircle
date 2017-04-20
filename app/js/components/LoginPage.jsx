89e88f985d9f520ea3d6eaf9bb4e712c1ebf063b
Shingo Lavine <shingolavine@gmail.com>
2017-04-20 18:45:39 -0400

    added middleware and login page state

--- app/js/components/LoginPage.jsx
+++ app/js/components/LoginPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Home from './Home.jsx';
+
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgsrc : this.props.imgsrc
+      imgsrc : this.props.imgsrc,
+      loggedin : this.props.loggedIn
     };
   }
   _handleLogin(event) {
@@ -16,42 +19,52 @@ export default class LoginPage extends React.Component {
       };
       
       $.post('/login', data, function(result, status) {
-        console.log(result);
-        console.log(status);
+        if (result == "Authorized") {
+          this.setState({loggedIn: true});
+        }
       });
 
   }
   
   render() {
-    return (
-      <div>
-        <form onSubmit={this._handleLogin.bind(this)}>
-        <div className="imgcontainer">
-          <img src={window.location.origin + "/" + this.state.imgsrc} alt="Avatar" className="avatar" />
-        </div>
-
-        <div className="form-group form-padding">
-          <label><b>Username</b></label>
-          <input className="form-control" id="username" type="text" placeholder="Enter Username" name="uname" required />
-
-          <label><b>Password</b></label>
-          <input className="form-control" id="password" type="password" placeholder="Enter Password" name="passwd" required />
-          <div className="form-padding-top">
-            <button className="btn btn-primary" type="submit">Login</button>
-            <span className="checkbox-right"><input type="checkbox" defaultChecked={false} /> Remember me</span>
+    if (!this.state.loggedIn) {
+      return (
+        <div>
+          <form onSubmit={this._handleLogin.bind(this)}>
+          <div className="imgcontainer">
+            <img src={window.location.origin + "/" + this.state.imgsrc} alt="Avatar" className="avatar" />
           </div>
 
-          <div className="form-padding-top form-padding-negative">
-            <span className="psw">Forgot <a href="#">password?</a></span>
+          <div className="form-group form-padding">
+            <label><b>Username</b></label>
+            <input className="form-control" id="username" type="text" placeholder="Enter Username" name="uname" required />
+
+            <label><b>Password</b></label>
+            <input className="form-control" id="password" type="password" placeholder="Enter Password" name="passwd" required />
+            <div className="form-padding-top">
+              <button className="btn btn-primary" type="submit">Login</button>
+              <span className="checkbox-right"><input type="checkbox" defaultChecked={false} /> Remember me</span>
+            </div>
+
+            <div className="form-padding-top form-padding-negative">
+              <span className="psw">Forgot <a href="#">password?</a></span>
+            </div>
           </div>
+        </form>
         </div>
-      </form>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div>
+          <Home/>
+        </div>
+        );
+    }
   }
 }
 
 LoginPage.defaultProps={
   imgsrc:'img_avatar2.png',
-  loginDisplay : false
+  loggedIn : false
 };
--- server/app.js
+++ server/app.js
@@ -228,6 +228,20 @@ function isValidUnhashedPassword(input, password) {
 	return (input === password);
 }
 
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+// Protected routes could be written like so:
+// app.get('/protected', isLoggedIn, function(req, res) { whatever })
+
 app.post('/login', 
   passport.authenticate('login', {
     successRedirect: '/',
