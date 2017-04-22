9a63b767089d88ee9c0e40238db55da30286ce86
edwardcgeorge <xuenan_li@brown.edu>
2017-04-21 20:29:08 -0400

    fix search result redirect

--- app/js/components/SearchList.jsx
+++ app/js/components/SearchList.jsx
@@ -8,10 +8,10 @@ export default class SearchList extends React.Component {
                 {
                     this.props.items.map(function(item) {
                         if(item.category === 'C') {
-                            return <a href={'charity'}><li key={item}>{item.name}</li></a>
+                            return <a href={'http://localhost:8080/charity'}><li key={item}>{item.name}</li></a>
                         }
                         else if(item.category === 'D') {
-                            return <a href={'donor'}><li key={item}>{item.name}</li></a>
+                            return <a href={'http://localhost:8080/donor'}><li key={item}>{item.name}</li></a>
                         }
 
                         {/*return <a href={'charity/'+item}><li key={item}>{item}</li></a>*/} //use this when to specific page
