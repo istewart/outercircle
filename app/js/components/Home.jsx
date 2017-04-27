b3a7ff3dd788fa499a6d11cdbc0c5088694a1263
istewart <istewart3141@gmail.com>
2017-04-26 22:56:19 -0400

    Changed what appears on the home page slightly.

--- app/js/components/Home.jsx
+++ app/js/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Feed from './Feed.jsx';
 import Navbar from './Navbar.jsx';
-import Stats from './Stats.jsx';
+import Suggestion from './Suggestion.jsx';
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -15,10 +15,11 @@ export default class Home extends React.Component {
         <Navbar/>
         <div id="main" className="center-block">
             <div className="col-md-4 col-sm-4 col-sm-push-7">
-                <Stats/>
+                <Suggestion type="donor"/>
+                <Suggestion type="charity"/>
             </div>
             <div className="col-md-6 col-sm-6 col-sm-pull-3">
-              <Feed/>
+                <Feed/>
             </div>
         </div>
       </div>
