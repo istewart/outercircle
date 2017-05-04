d5eb829f4c5719012f352d73f8e9e051ff28b328
Shingo Lavine <shingolavine@gmail.com>
2017-05-03 21:25:49 -0400

    fixed some formatting

--- app/js/components/EditProfile.jsx
+++ app/js/components/EditProfile.jsx
@@ -45,16 +45,21 @@ export default class EditProfile extends React.Component {
 			<div>
 				<button id="editProfile" className="btn btn-info pull-right" onClick={this._buttonClick.bind(this)}>Edit Profile</button>
 				<div className={style}>
-					<div class="modal-content animate">
-						<div class="modal-body">
-							<input className="form-control" id="donorName" type="text" value={this.state.name}/>
-							<input className="form-control" id="donorDescription" type="text" value={this.state.description}/>
-						</div>
-						<div class="modal-footer">
-							<button className="btn btn-primary" onClick={this._close.bind(this)}>cancel</button>
-							<button className="btn btn-primary" type="submit" onClick={this._submit.bind(this)}>save info</button>
-						</div>
-					</div>
+					<form className="modal-content animate">
+					    <div className="form-group form-padding">
+						    <div className="modal-body form-group form-padding">
+								<label>Donor Name</label>
+								<input className="form-control" id="donorName" type="text" defaultValue={this.state.name}/>
+								<label>Description</label>
+								<input className="form-control" id="donorDescription" type="text" defaultValue={this.state.description}/>
+							</div>
+
+						    <div className="form-padding-top form-padding-negative">
+						    	<button className="btn btn-primary" onClick={this._close.bind(this)}>cancel</button>
+								<button className="btn btn-primary" type="submit" onClick={this._submit.bind(this)}>save info</button>
+						    </div>
+					    </div>
+				  	</form>
 				</div>
 			</div>
 		);
