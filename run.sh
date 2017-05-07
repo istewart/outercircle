234c7b2261f504266e4db9e205bc36ed2ce451a5
Ian Stewart <istewart3141@gmail.com>
2017-05-07 17:28:47 -0400

    Added instructions for setting up on AWS.

---
+++ run.sh
@@ -0,0 +1,15 @@
+# install dependencies
+npm install
+
+# stop all the mean processes
+sudo /opt/bitnami/ctlscript.sh stop
+
+# enable ip forwarding from 80 to 8080
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
+
+# build and launch the server
+npm run boot
